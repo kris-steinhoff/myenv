96ff9d7d5aed70643795e2192b78baeebd966ea1
Kris Steinhoff <kris@ofa2.com>
2012-11-12 23:04:12 -0500

    working install script

--- install.sh
+++ install.sh
@@ -11,26 +11,70 @@ export COLOR_NONE="\e[0m"
 
 function notice()
 {
-    msg=${*}
-    echo -e "${COLOR_GREEN}${msg}${COLOR_NONE}"
+    echo "${COLOR_GREEN}${*}${COLOR_NONE}"
 }
 
 function warning()
 {
-    msg=${*}
-    echo -e "${COLOR_YELLOW}WARNING ${msg}${COLOR_NONE}"
+    echo "${COLOR_YELLOW}${*}${COLOR_NONE}"
 }
 
 function error()
 {
-    msg=${*}
-    echo -e "${COLOR_RED}ERROR ${msg}${COLOR_NONE}"
-    exit 1;
+    echo "${COLOR_RED}ERROR ${*}${COLOR_NONE}"
 }
 
-RC="$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/rc"
+function set_link()
+{
+    s=${1}
+    t=${2}
+    ln_output=`ln -s ${s} ${t} 2>&1`; r=$?
+    if [ $r -eq 0 ]; then
+        notice "linked ${s} to ${t}"
+    else
+        echo ${ln_output} | grep "File exists" &> /dev/null &&
+        warning "${t} already exists" ||
+        error "could not link ${s} to ${t}"
+    fi
+}
 
-RC_INSERT="test -r ${RC} && source ${RC}"
+MYENV_DIR="$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)"
 
-echo $RC_INSERT
+RC_INSERT="test -r ${MYENV_DIR}/rc && source ${MYENV_DIR}/rc"
+grep "^${RC_INSERT}$" ${BASHRC} &> /dev/null; r=$?
+if [ $r -eq 0 ]; then
+    warning "${MYENV_DIR}/rc already sourced in ${BASHRC}"
+else
+    echo ${RC_INSERT} >> ${BASHRC} &&
+    notice "${MYENV_DIR}/rc sourced in ${BASHRC}" ||
+    notice "could not source ${MYENV_DIR}/rc in ${BASHRC}"
+fi
 
+set_link $MYENV_DIR/screen-session-attach/attach ${HOME}/bin/attach
+set_link $MYENV_DIR/dotfiles/vimrc ${HOME}/.vimrc
+set_link $MYENV_DIR/dotfiles/screenrc ${HOME}/.screenrc
+set_link $MYENV_DIR/dotfiles/inputrc ${HOME}/.inputrc
+set_link $MYENV_DIR/dotfiles/ssh/rc ${HOME}/.ssh/rc
+set_link $MYENV_DIR/dotfiles/gitexcludes ${HOME}/.gitexcludes
+
+if [ -e "${HOME}/.gitconfig" ]; then
+    warning "${HOME}/.gitconfig already exists"
+else
+    cp ${MYENV_DIR}/dotfiles/gitconfig ${HOME}/.gitconfig &&
+    notice "copied ${MYENV_DIR}/dotfiles/gitconfig to ${HOME}/.gitconfig" ||
+    error "could not copy ${MYENV_DIR}/dotfiles/gitconfig to ${HOME}/.gitconfig"
+
+    git config --global core.excludesfile ${HOME}/.gitexcludes &&
+    notice "added ${HOME}/.gitexcludes to core.excludesfile in ${HOME}/.gitconfig" ||
+    error "could not add ${HOME}/.gitexcludes to core.excludesfile in ${HOME}/.gitconfig"
+
+    if [ "x${USER}" = "xkris" ]; then
+        git_user_email="kris@ofa2.com"
+    elif [ "x${USER}" = "xsteinhof" ]; then
+        git_user_email="steinhof@umich.edu"
+    fi
+
+    git config --global user.email ${git_user_email} &&
+    notice "added ${git_user_email} to user.email in ${HOME}/.gitconfig" ||
+    error "could not add ${git_user_email} to user.email in ${HOME}/.gitconfig"
+fi
