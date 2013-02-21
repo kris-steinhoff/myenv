81912df2a443782a93cb81ef9d9f4db730d31f1d
KRISofA2 <steinhof@umich.edu>
2013-02-20 21:11:19 -0500

    fixed path

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 # use these commands in a new home directory to bootstrap it
 
 cd ${HOME}
-echo 'test -r ${HOME}/.bashrc && source ${HOME}/.bashrc' >> ${HOME}/bash_profile
+echo 'test -r ${HOME}/.bashrc && source ${HOME}/.bashrc' >> ${HOME}/.bash_profile
 mkdir src bin
 cd src
 git clone git://github.com/KRISofA2/myenv.git ${HOME}/src/myenv
